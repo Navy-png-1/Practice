5f114cd18ae37fd9ddba55870e4ab3925adcbbd3
Navy-png-1 <navythewriter09@gmail.com>
2026-05-23 19:00:51 +0000

    start, pause, style functions. display

--- practice/app/page.tsx
+++ practice/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { setReactDebugChannelForHtmlRequest } from 'next/dist/server/dev/debug-channel'
 //import Image from "next/image";
 import { useState, useRef, useEffect } from 'react'
 
@@ -9,8 +10,32 @@ export default function Home() {
   const [sec, setSec] = useState(0)
   const [mili, setMili] = useState(0)
   const [time, setTime] = useState(0)
+  const [start, setStart] = useState(false)
+  const [run, setRun] = useState(false)
   const intervalRef = useRef(null)
   const startTimeRef = useRef(0)
+
+  useEffect (() => {
+    setHour(Math.floor(time / 3600000))
+    setMin(Math.floor((time % 360000) / 60000))
+    setSec(Math.floor((time % 60000) / 1000))
+    setMili(time % 1000)
+  }, [time])
+
+  const Startwatch = () => {
+    startTimeRef.current = Date.now() - time;
+    intervalRef.current = setInterval(() => {
+      setTime(
+        (Date.now() - startTimeRef.current))
+    }, 10)
+    setRun(true)
+    setStart(true)
+  }
+  const pause = () => {
+    clearInterval(intervalRef.current)
+    setRun(false)
+  }
+
 
   function handleClick (){ 
     setCount(count + 1)
@@ -24,65 +49,16 @@ export default function Home() {
       <button onClick={handleClick}>
         clicked {count} times
       </button>
+      <div>
+        <h1>{hours.toString().padStart(2, '0')} : {minutes.toString().padStart(2, '0')} : {sec.toString().padStart(2, '0')} : {mili.toString().padStart(3, '0')} </h1>
+        <button className="start" onClick={Startwatch}>
+          Start Button
+        </button>
+        <br></br>
+        <button onClick={pause}>
+          Pause Button
+        </button>
+      </div>
     </div>
-    // <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    //   <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-    //     <Image
-    //       className="dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js logo"
-    //       width={100}
-    //       height={20}
-    //       priority
-    //     />
-    //     <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-    //       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-    //         To get started, edit the page.tsx file.
-    //       </h1>
-    //       <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-    //         Looking for a starting point or more instructions? Head over to{" "}
-    //         <a
-    //           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //           className="font-medium text-zinc-950 dark:text-zinc-50"
-    //         >
-    //           Templates
-    //         </a>{" "}
-    //         or the{" "}
-    //         <a
-    //           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //           className="font-medium text-zinc-950 dark:text-zinc-50"
-    //         >
-    //           Learning
-    //         </a>{" "}
-    //         center.
-    //       </p>
-    //     </div>
-    //     <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-    //       <a
-    //         className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         <Image
-    //           className="dark:invert"
-    //           src="/vercel.svg"
-    //           alt="Vercel logomark"
-    //           width={16}
-    //           height={16}
-    //         />
-    //         Deploy Now
-    //       </a>
-    //       <a
-    //         className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Documentation
-    //       </a>
-    //     </div>
-    //   </main>
-    // </div>
   );
 }
